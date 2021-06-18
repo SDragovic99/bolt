739bd4540ebe70fb707d1461ac4fe79073a19c51
Melanija Racic <racicmelanija@gmail.com>
2021-06-18 23:46:02 +0200

    Inicijalni commit

---
+++ Bolt/src/app/App.java
@@ -0,0 +1,16 @@
+package app;
+
+import static spark.Spark.port;
+import static spark.Spark.staticFiles;
+
+import java.io.File;
+
+public class App {
+
+	public static void main(String[] args) throws Exception{
+		port(8080);
+
+		staticFiles.externalLocation(new File("./static").getCanonicalPath());
+	}
+
+}
